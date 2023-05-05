21882a682ea545eaf7cb2eb933862e88575f026f
Fadlur Rohman <f4dlur@gmail.com>
2023-05-05 09:23:11 +0700

    test install and uninstall

--- plugins/testing/testing_plugin.php
+++ plugins/testing/testing_plugin.php
@@ -9,17 +9,34 @@ class TestingPlugin extends Plugin
 
     public function install($plugin_id)
     {
-        $this->Record->
-        setField("id", array('type' => "int",'size' => 10,'unsigned' => true,'auto_increment' => true))->
-        setField("name", array('type' => "varchar", 'size' => 255))->
-        setField("email", array('type' => "varchar", 'size' => 255))->
-        setField("phone", array('type' => "varchar", 'size' => 255))->
-        setKey(array("id"), "primary")->
-        create("testing_user");
+        if (!isset($this->Record)) {
+            Loader::loadComponents($this, ['Input', 'Record']);
+        }
+
+        try {
+            $this->Record->
+            setField("id", array('type' => "int", 'size' => 10, 'unsigned' => true, 'auto_increment' => true))->
+            setField("name", array('type' => "varchar", 'size' => 255))->
+            setField("email", array('type' => "varchar", 'size' => 255))->
+            setField("phone", array('type' => "varchar", 'size' => 255))->
+            setKey(array("id"), "primary")->
+            create("testing_user");
+        } catch (Exception $e) {
+            // Error adding... no permission?
+            $this->Input->setErrors(['db' => ['create' => $e->getMessage()]]);
+            return;
+        }
     }
 
     public function uninstall($plugin_id, $last_instance)
     {
-        $this->Record->drop('testing_user');
+        if (!isset($this->Record)) {
+            Loader::loadComponents($this, ['Record']);
+        }
+
+        // Remove all system_overview tables *IFF* no other company in the system is using this plugin
+        if ($last_instance) {
+            $this->Record->drop('testing_user');
+        }
     }
 }
