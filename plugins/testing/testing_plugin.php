7aaf49bca362cb029657e45358f27b72345ed319
Fadlur Rohman <f4dlur@gmail.com>
2023-05-05 09:16:48 +0700

    test install and uninstall

--- plugins/testing/testing_plugin.php
+++ plugins/testing/testing_plugin.php
@@ -6,4 +6,20 @@ class TestingPlugin extends Plugin
     {
         $this->loadConfig(dirname(__FILE__) . DS . 'config.json');
     }
+
+    public function install($plugin_id)
+    {
+        $this->Record->
+        setField("id", array('type' => "int",'size' => 10,'unsigned' => true,'auto_increment' => true))->
+        setField("name", array('type' => "varchar", 'size' => 255))->
+        setField("email", array('type' => "varchar", 'size' => 255))->
+        setField("phone", array('type' => "varchar", 'size' => 255))->
+        setKey(array("id"), "primary")->
+        create("testing_user");
+    }
+
+    public function uninstall($plugin_id, $last_instance)
+    {
+        $this->Record->drop('testing_user');
+    }
 }
