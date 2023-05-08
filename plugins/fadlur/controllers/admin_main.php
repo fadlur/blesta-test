e3b7adbe0abbe40a9892b340ba5e0f00edb9cfb2
Fadlur Rohman <f4dlur@gmail.com>
2023-05-08 23:06:13 +0700

    debug uri plugin action

--- plugins/fadlur/controllers/admin_main.php
+++ plugins/fadlur/controllers/admin_main.php
@@ -7,7 +7,7 @@ class AdminMain extends \FadlurController
     public function index()
     {
         $this->uses(["Clients"]);
-        $list_client = $this->Clients->getList();
+        $list_client = [];
         $this->set('list_client', $list_client);
 
         $this->view('admin_main');
