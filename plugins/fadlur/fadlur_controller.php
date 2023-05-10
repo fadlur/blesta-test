bfcb279f401fb75c1c156328f8a8fbd3bf54df4f
Fadlur Rohman <f4dlur@gmail.com>
2023-05-10 18:47:59 +0700

    debug view admin clients

--- plugins/fadlur/fadlur_controller.php
+++ plugins/fadlur/fadlur_controller.php
@@ -4,11 +4,11 @@ class FadlurController extends AppController
 {
     public function preAction()
     {
-        $this->structure->setDefaultView(APPDIR);
+//        $this->structure->setDefaultView(APPDIR);
         parent::preAction();
 
         $this->view->view = "default";
-        $this->structure->view = 'default';
+//        $this->structure->view = 'default';
     }
 
     public function index()
